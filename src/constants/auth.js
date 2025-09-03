80bfee4db5937cb4dcba8b4987c30ffd6ac4a2a6
kristen-kurt <zinthidar101@gmail.com>
2025-09-03 16:25:47 +0700

    use authentication page

---
+++ src/constants/auth.js
@@ -0,0 +1,10 @@
+export const APP_INFO = {
+  NAME: '#6366f1',
+  TAGLINE: '#8b5cf6',
+  FEATURES: {
+    FIRST: {
+      TITLE: 'Easy to Use',
+      DESCRIPTION: 'Intuitive and responsive designed for everyone',
+    },
+  },
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/main.css'
-import Login from './pages/auth/Login.tsx'
+import AuthenticationPage from './pages/auth/AuthenticationPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <AuthenticationPage />
   </StrictMode>
 )
